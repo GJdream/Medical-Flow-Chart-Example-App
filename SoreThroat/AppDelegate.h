76944f23862b57a817b5c293e529f467e3bac21c
Mark Hambly <hambly@gmail.com>
2013-09-05 09:25:24 +1200

    Initial Commit

---
+++ SoreThroat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SoreThroat
+//
+//  Created by Mark on 9/5/13.
+//  Copyright (c) 2013 Mark Hambly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SoreThroat/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SoreThroat
+//
+//  Created by Mark on 9/5/13.
+//  Copyright (c) 2013 Mark Hambly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
